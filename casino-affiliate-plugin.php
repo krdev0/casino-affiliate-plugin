96d3e05310f975198b24b45a8b236c11bb610cf8
Kristaps Ritins <kritinsdev@gmail.com>
2021-12-16 23:24:07 +0200

    test

---
+++ casino-affiliate-plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: Casino Affiliate Plugin
+ * Description: Casino affiliate plugin. Create casino reviews.
+ * Author:49Prod
+ * Author URI: https://somesome.com/
+ * Version: 1.0
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
